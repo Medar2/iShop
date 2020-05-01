18697050ec45dc1be712cea2db6f574c069b9d8c
Jose Caraballo <jcaraballo74@hotmail.com>
2020-05-01 19:50:17 -0400

    Change appsetting

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -47,7 +47,7 @@ namespace Shop.Web
 
             services.AddDbContext<DataContext>(cfg =>
                 {
-                    cfg.UseSqlServer(this.Configuration.GetConnectionString("ShopConnection"));
+                    cfg.UseSqlServer(this.Configuration.GetConnectionString("ProConnection"));
                 });
 
             services.AddTransient<SeedDb>();
